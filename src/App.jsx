5a3460332e92c3772dd5669761530d6c03d1e8df
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-04-24 13:42:52 +0530

    added font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 function App() {
   return (
     <div>
       <p>lol</p>
+      <FontAwesomeIcon icon = {faSearch} />
     </div>
   );
 }
