2d3cec1f550409cdd854ac64c89ea7274417d8db
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-05-18 14:03:00 +0530

    booking page

--- src/admin/bookings/bookings.jsx
+++ src/admin/bookings/bookings.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import { URL } from '../../url';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 class BookingsAdmin extends React.Component {
-    constructor(props) {
-        super(props); 
-        this.state = {
-            bookings: []
-        }
-    }
-
-    async componentDidMount() {
-        await axios.get(URL+'bookings')
-        .then(res => {
-            this.setState({
-                bookings: res.data
-            })
-        })
-    }
 
     render() {
-        return <div>BOOKINGS</div>
+        return (
+            <div style={{ padding: 4+'%' }}>
+                <Link to="bookings/create">
+                    <Button block>Create Booking</Button>
+                </Link>
+            </div>
+        );
     }
 }
 
