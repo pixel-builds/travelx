2e5fadee9c1d425caafb9995d38b8d6f7de4e698
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-05-19 16:05:44 +0530

    added registry verified and booked pages

--- src/admin/registry/bookedRegistries.jsx
+++ src/admin/registry/bookedRegistries.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import { URL } from '../../url';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-class bookedRegistries extends React.Component {
+export class BookedRegistries extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -12,21 +14,39 @@ class bookedRegistries extends React.Component {
 
     async componentDidMount() {
         const token = await JSON.parse(window.localStorage.getItem("token"));
-        await axios.get(URL+'registry/booked', {
-            headers: {'Authorization': 'Bearer '+ token}
+        await axios.get(URL + 'registry/booked', {
+            headers: { 'Authorization': 'Bearer ' + token }
         }).then(res => {
             this.setState({
                 registries: res.data
             })
         })
     }
+
     render() {
         return (
-            <div>
-                BOOKED REGIStries           
+            <div style={{ padding: 3+'%', display: 'block' }}>
+                <h1 className="heading"> Booked Registries </h1>
+                {
+                    this.state.registries.map((item, key) => {
+                        return (
+                            <Card key={key} style={{ marginTop: 6+'%' }}>
+                            <Card.Body>
+                                <Card.Title>{item._id}</Card.Title>
+                                <Card.Title>Category: {item.category}</Card.Title>
+                                <div>
+                                    <Link to={"/admin/registry/"+item._id}>Details</Link>
+                                    <Link style={{ marginLeft:20+'px' }} to="/">User</Link>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                        );
+                    })
+                }
+                <p><br/></p>
             </div>
-        );
+        )
     }
 }
 
-export default bookedRegistries
+export default BookedRegistries
--- src/admin/registry/verifiedRegistries.jsx
+++ src/admin/registry/verifiedRegistries.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import { URL } from '../../url';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-class VerifiedRegistries extends React.Component  {
+export class VerifiedRegistries extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -12,8 +14,8 @@ class VerifiedRegistries extends React.Component  {
 
     async componentDidMount() {
         const token = await JSON.parse(window.localStorage.getItem("token"));
-        await axios.get(URL+'registry/verified', {
-            headers: {'Authorization': 'Bearer '+ token}
+        await axios.get(URL + 'registry/verified', {
+            headers: { 'Authorization': 'Bearer ' + token }
         }).then(res => {
             this.setState({
                 registries: res.data
@@ -23,11 +25,28 @@ class VerifiedRegistries extends React.Component  {
 
     render() {
         return (
-            <div>
-                VerifiedRegistries
+            <div style={{ padding: 3+'%', display: 'block' }}>
+                <h1 className="heading">Verified Registries </h1>
+                {
+                    this.state.registries.map((item, key) => {
+                        return (
+                            <Card key={key} style={{ marginTop: 6+'%' }}>
+                            <Card.Body>
+                                <Card.Title>{item._id}</Card.Title>
+                                <Card.Title>Category: {item.category}</Card.Title>
+                                <div>
+                                    <Link to={"/admin/registry/"+item._id}>Details</Link>
+                                    <Link style={{ marginLeft:20+'px' }} to="/">User</Link>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                        );
+                    })
+                }
+                <p><br/></p>
             </div>
         )
     }
 }
 
-export default VerifiedRegistries;
+export default VerifiedRegistries
