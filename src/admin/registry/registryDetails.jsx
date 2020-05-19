3cfd92a82a71d8c9d7eb19e453a6d8f93511ef52
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-05-19 15:59:22 +0530

    added registry details requests

--- src/admin/registry/registryDetails.jsx
+++ src/admin/registry/registryDetails.jsx
@@ -6,7 +6,9 @@ export default class RegistryDetails extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            registry: {}
+            registry: {},
+            booking: {},
+            user: {}
         }
     }
 
@@ -15,8 +17,19 @@ export default class RegistryDetails extends React.Component {
         const {match: {params}} = this.props;
         await axios.get(URL+'registry/'+params.id, {
             headers: {'Authorization': 'Bearer '+token}
-        }).then(res => {
+        }).then(async res => {
             this.setState({registry: res.data})
+            await axios.get(URL+'bookings/'+this.state.registry.bookingId)
+            .then(res => {
+                this.setState({booking: res.data})
+            }).catch(e => {
+                console.log(e)
+            })
+            await axios.get(URL+'auth/users/'+ this.state.registry.userId, {
+                headers: { 'Authorization': 'Bearer '+token }
+            }).then(res => {
+                this.setState({ user: res.data })
+            })
         }).catch(e => {
             console.log(e);
         })
@@ -25,7 +38,8 @@ export default class RegistryDetails extends React.Component {
     render() {
         return (
             <div>
-                DETAILS
+                <h1 className="heading">{this.state.booking.name}</h1>
+                <p>Booked By -> {this.state.user.name}</p>
             </div>
         )
     }
